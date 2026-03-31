4aef99d8b00b826cb4a50442bd9c4b2ac9e62fb0
naibhivenkat <venkat.naibhi@gmail.com>
2026-03-31 13:52:06 +0530

    Tournaments Routes added

--- app/tournaments/tournament_routes.py
+++ app/tournaments/tournament_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.database import models
@@ -6,20 +7,22 @@ from app.database.db import get_db
 
 router = APIRouter(prefix="/tournaments")
 
-
+class TournamentCreate(BaseModel):
+    name: str
+    format: str
+    overs: int
 @router.post("/create")
-def create_tournament(name: str, format: str, overs: int, db: Session = Depends(get_db)):
+def create_tournament(data: TournamentCreate, db: Session = Depends(get_db)):
     t = models.Tournament(
-        name=name,
-        format=format,
-        overs=overs
+        name=data.name,
+        format=data.format,
+        overs=data.overs
     )
     db.add(t)
     db.commit()
     db.refresh(t)
 
     return {"id": t.id, "message": "Tournament created"}
-
 
 @router.post("/{tournament_id}/add_team")
 def add_team(tournament_id: int, team_id: int, db: Session = Depends(get_db)):
