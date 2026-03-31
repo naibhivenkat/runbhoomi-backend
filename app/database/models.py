03754e9821cf479169d76125b13a47d5d35e4d26
naibhivenkat <venkat.naibhi@gmail.com>
2026-03-31 11:50:03 +0530

    Missing veriable name fix 3

--- app/database/models.py
+++ app/database/models.py
@@ -224,9 +224,37 @@
 
 
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from app.database.db import Base
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String(100), nullable=False)
+    phone = Column(String(15), nullable=False)
+    email = Column(String(120), unique=True, index=True, nullable=False)
+    gender = Column(String(10), nullable=True)
+
+    city = Column(String, nullable=True, index=True)
+    role = Column(String, nullable=True, index=True)
+
+    batting_style = Column(String, nullable=True)
+    bowling_style = Column(String, nullable=True)
+
+    experience = Column(Integer, default=0)
+    jersey_number = Column(Integer, default=0)
+
+    dob = Column(Date, nullable=True)
+
+    password_hash = Column(String, nullable=False)
+    profile_photo = Column(String, nullable=True)
+    team_id = Column(Integer, ForeignKey("teams.id"), nullable=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 
 class Match(Base):
