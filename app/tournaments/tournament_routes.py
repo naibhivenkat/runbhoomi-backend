8edc290c6e30052393519f33b6c26e6a456f3310
naibhivenkat <venkat.naibhi@gmail.com>
2026-03-31 13:52:35 +0530

    Tournaments Routes added 2

--- app/tournaments/tournament_routes.py
+++ app/tournaments/tournament_routes.py
@@ -7,10 +7,13 @@ from app.database.db import get_db
 
 router = APIRouter(prefix="/tournaments")
 
+
 class TournamentCreate(BaseModel):
     name: str
     format: str
     overs: int
+
+
 @router.post("/create")
 def create_tournament(data: TournamentCreate, db: Session = Depends(get_db)):
     t = models.Tournament(
@@ -23,6 +26,7 @@ def create_tournament(data: TournamentCreate, db: Session = Depends(get_db)):
     db.refresh(t)
 
     return {"id": t.id, "message": "Tournament created"}
+
 
 @router.post("/{tournament_id}/add_team")
 def add_team(tournament_id: int, team_id: int, db: Session = Depends(get_db)):
